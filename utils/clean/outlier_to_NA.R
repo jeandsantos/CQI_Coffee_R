65b305af1e70a1a973dc0d4af05f8286faadd4c2
Jean Dos Santos <jeandsantos88@gmail.com>
2020-08-16 17:34:50 +0100

    completed processing altitude

--- utils/clean/outlier_to_NA.R
+++ utils/clean/outlier_to_NA.R
@@ -1,6 +1,6 @@
 outlier_to_NA <- function(x, min_threshold = 0, max_threshold = Inf){
   
-  x[(x > max_threshold) & (x < min_threshold) & (!is.na(x))] <- NA
+  x[((x > max_threshold) | (x < min_threshold)) & (!is.na(x))] <- NA
   
   return(x)
   
---
+++ utils/convert/convert_ft_to_m.R
@@ -0,0 +1,1 @@
+convert_ft_to_m <- function(x, dec = 2){ return( round(x*0.3048000, 2) ) }
